089e610611f8028af5974bea49ab9e9f33180391
OsamaHaydar05 <osamahaydar05@gmail.com>
2025-05-18 21:34:43 +0200

    Sale.java

--- src/se/kth/iv1350/possystem/model/Sale.java
+++ src/se/kth/iv1350/possystem/model/Sale.java
@@ -2,16 +2,31 @@ package model;
 
 import integration.ItemDTO;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Collection;
 
 public class Sale {
     private HashMap<String, SoldItem> soldItems = new HashMap<>();
 
-    public void addItem(ItemDTO item) {
+    
+    public void addItem(ItemDTO item) {  // Metod för 1 artikel åt gången
         SoldItem sold = soldItems.get(item.id);
         if (sold == null) {
             soldItems.put(item.id, new SoldItem(item, 1));
         } else {
             sold.increaseQuantity();
+        }
+    }
+
+    
+    public void addItem(ItemDTO item, int quantity) {     // Metod för flera artiklar direkt
+        SoldItem sold = soldItems.get(item.id);
+        if (sold == null) {
+            soldItems.put(item.id, new SoldItem(item, quantity));
+        } else {
+            for (int i = 0; i < quantity; i++) {
+                sold.increaseQuantity();
+            }
         }
     }
 
@@ -30,4 +45,9 @@ public class Sale {
             .mapToDouble(SoldItem::getTotalVAT)
             .sum();
     }
-} 
+
+
+    public Collection<SoldItem> getSoldItems() {    // Kvitto från View
+        return soldItems.values();
+    }
+}
