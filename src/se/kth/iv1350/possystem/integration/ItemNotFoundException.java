66173ef29f84337457b383321a143a2fab629a5c
OsamaHaydar05 <osamahaydar05@gmail.com>
2025-05-18 22:10:00 +0200

    ItemNotFoundException.java

--- src/se/kth/iv1350/possystem/integration/ItemNotFoundException.java
+++ src/se/kth/iv1350/possystem/integration/ItemNotFoundException.java
@@ -1,1 +1,7 @@
+package integration;
 
+public class ItemNotFoundException extends RuntimeException {
+    public ItemNotFoundException(String itemId) {
+        super("Item with ID '" + itemId + "' was not found.");
+    }
+}
