02781f9f8f1306bcb059a2b526f09b06713a1754
OsamaHaydar05 <osamahaydar05@gmail.com>
2025-05-18 22:02:41 +0200

    SaleTest.java

--- test/se/kth/iv1350/possystem/model/SaleTest.java
+++ test/se/kth/iv1350/possystem/model/SaleTest.java
@@ -1,6 +1,8 @@
 package model;
 
+import integration.ItemDTO;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SaleTest {
@@ -8,12 +10,33 @@ public class SaleTest {
     @Test
     public void testAddItemIncreasesTotal() {
         Sale sale = new Sale();
-        Item item = new Item("abc123", "BigWheel Oatmeal", 29.90, 0.06, "500g");
+        ItemDTO item = new ItemDTO("abc123", "BigWheel Oatmeal", 29.90, 0.06, "500g");
         sale.addItem(item);
 
         double expectedTotal = 29.90;
         double actualTotal = sale.getRunningTotal();
 
-        assertEquals(expectedTotal, actualTotal, 0.01, "The total should be equal to the item price.");
+        assertEquals(expectedTotal, actualTotal, 0.01, "The total should be equal to the item price");
+    }
+
+    @Test
+    public void testAddSameItemTwice() {
+        Sale sale = new Sale();
+        ItemDTO item = new ItemDTO("abc123", "BigWheel Oatmeal", 29.90, 0.06, "500g");
+        sale.addItem(item);
+        sale.addItem(item);
+
+        double expected = 2 * 29.90;
+        assertEquals(expected, sale.getRunningTotal(), 0.01, "Total should double when same item is added twice");
+    }
+
+    @Test
+    public void testAddItemWithQuantity() {
+        Sale sale = new Sale();
+        ItemDTO item = new ItemDTO("def456", "YouGoGo Blueberry", 14.90, 0.06, "240g");
+        sale.addItem(item, 3);
+
+        double expected = 3 * 14.90;
+        assertEquals(expected, sale.getRunningTotal(), 0.01, "Total should match the quantity");
     }
 }
