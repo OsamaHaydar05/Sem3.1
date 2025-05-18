54732b5d04e50d2b7a403d84199098757405cf1e
OsamaHaydar05 <osamahaydar05@gmail.com>
2025-05-18 22:00:57 +0200

    ControllerTest.java

--- test/se/kth/iv1350/possystem/controller/ControllerTest.java
+++ test/se/kth/iv1350/possystem/controller/ControllerTest.java
@@ -1,29 +1,37 @@
 package controller;
 
-import integration.InventorySystem;
-import integration.AccountingSystem;
-import integration.Printer;
-import integration.ItemRegistry;
+import integration.ItemDTO;
 import model.Sale;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ControllerTest {
 
     @Test
     public void testStartSaleCreatesSaleObject() {
-        Controller controller = new Controller(
-            new InventorySystem(),
-            new AccountingSystem(),
-            new Printer(),
-            new ItemRegistry()
-        );
+        Controller controller = new Controller();
+        controller.startNewSale();
 
-        controller.startSale();
+        Sale sale = controller.getCurrentSale();
+        assertNotNull(sale, "Controller should create a Sale object when startNewSale is called.");
+    }
 
-        
-        Sale sale = controller.getCurrentSale();  
+    @Test
+    public void testRegisterItemIncreasesTotal() {
+        Controller controller = new Controller();
+        controller.startNewSale();
 
-        assertNotNull(sale, "Controller should create a Sale object when startSale is called.");
+        controller.registerItem("abc123");
+        assertTrue(controller.getRunningTotal() > 0, "Total increase after adding an item");
+    }
+
+    @Test
+    public void testRegisterItemWithQuantity() {
+        Controller controller = new Controller();
+        controller.startNewSale();
+
+        controller.registerItem("def456", 3);
+        assertTrue(controller.getRunningTotal() >= 3 * 14.90, "Total should increase according to quantity");
     }
 }
