307438d11b4ca63384d0d689d67d2c66b71e7556
OsamaHaydar05 <osamahaydar05@gmail.com>
2025-05-18 21:38:24 +0200

    View.java

--- src/se/kth/iv1350/possystem/view/View.java
+++ src/se/kth/iv1350/possystem/view/View.java
@@ -3,6 +3,7 @@ package view;
 import controller.Controller;
 import integration.ItemDTO;
 import model.Receipt;
+import model.SoldItem;
 
 public class View {
     private Controller controller;
@@ -14,25 +15,40 @@ public class View {
     public void run() {
         controller.startNewSale();
 
-        System.out.println("Add 1 item with item id abc123:");
+        
+        System.out.println("Add 1 item with item id abc123:"); // En vara skannas två gånger
         ItemDTO item1 = controller.registerItem("abc123");
         printItemDetails(item1);
-        printRunningTotal(controller);
+        printRunningTotal();
 
-        System.out.println("\nAdd 1 item with item id abc123:");
+        System.out.println("\nAdd 1 more item with item id abc123:");
         ItemDTO item2 = controller.registerItem("abc123");
         printItemDetails(item2);
-        printRunningTotal(controller);
+        printRunningTotal();
 
-        System.out.println("\nAdd 1 item with item id def456:");
-        ItemDTO item3 = controller.registerItem("def456");
+        
+        System.out.println("\nAdd 3 items with item id def456:");   // Registrera 3 st av samma vara direkt
+        ItemDTO item3 = controller.registerItem("def456", 3);
         printItemDetails(item3);
-        printRunningTotal(controller);
+        printRunningTotal();
 
         System.out.println("\nEnd sale:");
-        Receipt receipt = controller.endSale(100.0);
-        System.out.println(receipt);
-        System.out.println("Change to give the customer: " + String.format("%.2f", receipt.getChange()) + " SEK");
+        Receipt receipt = controller.endSale(200.0);
+
+        
+        System.out.println("\n🧾 Receipt:");
+        for (SoldItem soldItem : receipt.getSoldItems()) {  //kriv ut varje kvittorad med korrekt format
+            System.out.println(
+                soldItem.getItem().name + "  " +
+                soldItem.getQuantity() + " x " + String.format("%.2f", soldItem.getItem().price) + " SEK" +
+                " = " + String.format("%.2f", soldItem.getTotalPrice()) + " SEK"
+            );
+        }
+
+    
+        System.out.println("Total: " + String.format("%.2f", receipt.getTotalPrice()) + " SEK"); // Totalsumma
+        System.out.println("Total VAT: " + String.format("%.2f", receipt.getTotalVAT()) + " SEK");
+        System.out.println("Change: " + String.format("%.2f", receipt.getChange()) + " SEK");
     }
 
     private void printItemDetails(ItemDTO item) {
@@ -43,7 +59,7 @@ public class View {
         System.out.println("Item description: " + item.description);
     }
 
-    private void printRunningTotal(Controller controller) {
+    private void printRunningTotal() {
         System.out.println("\nTotal cost (incl VAT): " + String.format("%.2f", controller.getRunningTotal()) + " SEK");
         System.out.println("Total VAT: " + String.format("%.2f", controller.getTotalVAT()) + " SEK");
     }
