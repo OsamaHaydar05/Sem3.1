f9d70ba4d15bdf7cf9449166b62f9967bdbd0b69
OsamaHaydar05 <osamahaydar05@gmail.com>
2025-05-19 16:42:14 +0200

    ControllerTest.java

--- sem4/test/se/kth/iv1350/possystem/controller/ControllerTest.java
+++ sem4/test/se/kth/iv1350/possystem/controller/ControllerTest.java
@@ -1,9 +1,8 @@
 package controller;
 
-import integration.InventorySystem;
-import integration.AccountingSystem;
-import integration.Printer;
-import integration.ItemRegistry;
+import integration.ItemDTO;
+import integration.ItemNotFoundException;
+import integration.InventoryDatabaseException;
 import model.Sale;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,29 +11,41 @@ public class ControllerTest {
 
     @Test
     public void testStartSaleCreatesSaleObject() {
-        Controller controller = new Controller(
-            new InventorySystem(),
-            new AccountingSystem(),
-            new Printer(),
-            new ItemRegistry()
-        );
+        Controller controller = new Controller();
+        controller.startNewSale();
 
-        controller.startSale();
-
-        
-        Sale sale = controller.getCurrentSale();  
-
-        assertNotNull(sale, "Controller should create a Sale object when startSale is called.");
+        Sale sale = controller.getCurrentSale();
+        assertNotNull(sale, "Controller should create a Sale object when startNewSale is called.");
     }
 
     @Test
-public void testItemNotFoundThrowsException() {
-    Controller controller = new Controller();
-    controller.startNewSale();
+    public void testItemNotFoundThrowsException() {
+        Controller controller = new Controller();
+        controller.startNewSale();
 
-    assertThrows(ItemNotFoundException.class, () -> {
-        controller.registerItem("nonexistentId");
-    });
-}
+        assertThrows(ItemNotFoundException.class, () -> {
+            controller.registerItem("nonexistentId");
+        });
+    }
 
+    @Test
+    public void testInventoryDatabaseExceptionIsThrown() {
+        Controller controller = new Controller();
+        controller.startNewSale();
+
+        assertThrows(InventoryDatabaseException.class, () -> {
+            controller.registerItem("dbError"); // ska simulera fel
+        });
+    }
+
+    @Test
+    public void testRegisterItemWithQuantity() throws ItemNotFoundException, InventoryDatabaseException {
+        Controller controller = new Controller();
+        controller.startNewSale();
+
+        ItemDTO item = controller.registerItem("abc123", 3);
+
+        assertNotNull(item, "Item should be found and not null.");
+        assertEquals("abc123", item.id, "Item ID should match.");
+    }
 }
