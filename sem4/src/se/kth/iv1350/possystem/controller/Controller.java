e0fe82897769876ae44eaedd79ca9aecf93abccd
OsamaHaydar05 <osamahaydar05@gmail.com>
2025-05-19 14:28:44 +0200

    Controller.java

--- sem4/src/se/kth/iv1350/possystem/controller/Controller.java
+++ sem4/src/se/kth/iv1350/possystem/controller/Controller.java
@@ -4,6 +4,8 @@ import integration.ExternalInventorySystem;
 import integration.ItemDTO;
 import model.Sale;
 import model.Receipt;
+import integration.ItemNotFoundException;
+import integration.InventoryDatabaseException;
 
 public class Controller {
     private Sale currentSale;
@@ -17,11 +19,12 @@ public class Controller {
         currentSale = new Sale();
     }
 
-    public ItemDTO registerItem(String itemId) {
-        ItemDTO item = inventory.getItem(itemId);
-        currentSale.addItem(item);
-        return item;
-    }
+    public ItemDTO registerItem(String itemId)
+        throws ItemNotFoundException, InventoryDatabaseException {
+    ItemDTO item = inventory.getItem(itemId);
+    currentSale.addItem(item);
+    return item;
+}
 
     public Receipt endSale(double amountPaid) {
         return currentSale.finalizeSale(amountPaid);
