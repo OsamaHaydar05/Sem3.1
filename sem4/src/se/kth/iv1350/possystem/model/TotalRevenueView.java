3e748b72c57e864bd4ce7233c0068c343a75aafa
OsamaHaydar05 <osamahaydar05@gmail.com>
2025-05-19 14:34:01 +0200

    TotalRevenueView.java

---
+++ sem4/src/se/kth/iv1350/possystem/model/TotalRevenueView.java
@@ -0,0 +1,11 @@
+package model;
+
+public class TotalRevenueView implements RevenueObserver {
+    private double totalRevenue = 0;
+
+    @Override
+    public void newRevenue(double amount) {
+        totalRevenue += amount;
+        System.out.println("🧾 Total revenue is now: " + String.format("%.2f", totalRevenue) + " SEK");
+    }
+}
