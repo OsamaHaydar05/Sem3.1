340331ceaadeb52aa2602b0be70bf831f7a4feca
OsamaHaydar05 <osamahaydar05@gmail.com>
2025-06-01 20:23:49 +0200

    Main.java

--- sem4/src/se/kth/iv1350/possystem/startup/Main.java
+++ sem4/src/se/kth/iv1350/possystem/startup/Main.java
@@ -1,12 +1,17 @@
 package startup;
 
-
 import controller.Controller;
 import view.View;
 
+
 public class Main {
+    /**
+     * Starts the program by creating the controller and the view.
+     *
+     * @param args Command-line arguments (not used).
+     */
     public static void main(String[] args) {
         Controller controller = new Controller();
         new View(controller);
     }
-} 
+}
