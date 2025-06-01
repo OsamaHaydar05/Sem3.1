9bc00bae82e7b3b024b846d7f4314e90c43cf4cc
OsamaHaydar05 <osamahaydar05@gmail.com>
2025-06-01 19:58:00 +0200

    View.java

--- sem4/src/se/kth/iv1350/possystem/view/View.java
+++ sem4/src/se/kth/iv1350/possystem/view/View.java
@@ -5,6 +5,9 @@ import integration.ItemDTO;
 import model.Receipt;
 import integration.ItemNotFoundException;
 import integration.InventoryDatabaseException;
+import integration.TotalRevenueView;
+import integration.TotalRevenueFileOutput;
+
 
 public class View {
     private Controller controller;
@@ -12,8 +15,9 @@ public class View {
     public View(Controller controller) {
         this.controller = controller;
         controller.startNewSale();
-        controller.addRevenueObserver(new model.TotalRevenueView()); 
-        controller.addRevenueObserver(new model.TotalRevenueFileOutput());
+        controller.addRevenueObserver(new TotalRevenueView());
+        controller.addRevenueObserver(new TotalRevenueFileOutput());
+
 
 
         System.out.println("Add 1 item with item id abc123:");
@@ -21,27 +25,36 @@ public class View {
         ItemDTO item1 = controller.registerItem("abc123");
         printItemDetails(item1);
         printRunningTotal(controller);
-} catch (ItemNotFoundException | InventoryDatabaseException e) {
-    System.out.println("ERROR: " + e.getMessage());
+}  catch (ItemNotFoundException e) {
+    System.out.println("ERROR: The item ID you entered was not found.");
+} catch (InventoryDatabaseException e) {
+    System.out.println("ERROR: Could not access the inventory system. Please try again later.");
 }
+
         
         System.out.println("\nAdd 1 item with item id abc123:");
         try {
         ItemDTO item2 = controller.registerItem("abc123");
         printItemDetails(item2);
         printRunningTotal(controller);
-} catch (ItemNotFoundException | InventoryDatabaseException e) {
-    System.out.println("ERROR: " + e.getMessage());
+}  catch (ItemNotFoundException e) {
+    System.out.println("ERROR: The item ID you entered was not found.");
+} catch (InventoryDatabaseException e) {
+    System.out.println("ERROR: Could not access the inventory system. Please try again later.");
 }
+
         
         System.out.println("\nAdd 1 item with item id def456:");
         try {
         ItemDTO item3 = controller.registerItem("def456", 3);
         printItemDetails(item3);
         printRunningTotal(controller);
-} catch (ItemNotFoundException | InventoryDatabaseException e) {
-    System.out.println("ERROR: " + e.getMessage());
+}  catch (ItemNotFoundException e) {
+    System.out.println("ERROR: The item ID you entered was not found.");
+} catch (InventoryDatabaseException e) {
+    System.out.println("ERROR: Could not access the inventory system. Please try again later.");
 }
+
         
         System.out.println("\nEnd sale:");
         Receipt receipt = controller.endSale(200.0); // Det kunden betalar
