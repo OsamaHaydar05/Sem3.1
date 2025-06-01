7a6bdd1e9d285a633de2ae8fbfdbf3247b452093
OsamaHaydar05 <osamahaydar05@gmail.com>
2025-06-01 20:21:35 +0200

    Receipt.java

--- sem4/src/se/kth/iv1350/possystem/model/Receipt.java
+++ sem4/src/se/kth/iv1350/possystem/model/Receipt.java
@@ -4,6 +4,10 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents a finalized receipt containing all details of a completed sale,
+ * including sold items, total price, VAT, paid amount, change, and time.
+ */
 public class Receipt {
     private final HashMap<String, SoldItem> soldItems;
     private final double amountPaid;
@@ -11,8 +15,13 @@ public class Receipt {
     private final double totalVAT;
     private final double change;
     private final LocalDateTime timeOfSale;
-    
 
+    /**
+     * Creates a new receipt with all items and payment details for a completed sale.
+     *
+     * @param soldItems A collection of items sold during the sale.
+     * @param amountPaid The amount of money paid by the customer.
+     */
     public Receipt(HashMap<String, SoldItem> soldItems, double amountPaid) {
         this.soldItems = soldItems;
         this.amountPaid = amountPaid;
@@ -22,10 +31,22 @@ public class Receipt {
         this.timeOfSale = LocalDateTime.now();
     }
 
+    /**
+     * Returns the calculated change that should be given to the customer.
+     *
+     * @return The amount of change.
+     */
     public double getChange() {
         return change;
     }
 
+    /**
+     * Returns a string representation of the entire receipt, including time of sale,
+     * list of sold items, total price, VAT, paid amount, and change.
+     *
+     * @return A formatted string representing the printed receipt.
+     */
+    @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("- - - - - - - - - - - - - - - - - - Begin receipt - - - - - - - - - - - - - - - - - - -\n");
@@ -38,4 +59,4 @@ public class Receipt {
         sb.append("- - - - - - - - - - - - - - - - - - End receipt - - - - - - - - - - - - - - - - - - - -\n");
         return sb.toString();
     }
-} 
+}
